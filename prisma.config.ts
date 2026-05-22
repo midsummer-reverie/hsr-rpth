a15e4380858c58d8b6a5a57ae5e4224d723ea2d1
midsummer-reverie <cccecilia.tz@gmail.com>
2026-05-23 01:34:04 +0700

    Remove migrate config to fix TS error

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,9 +7,6 @@ export default defineConfig({
   datasource: {
     url: process.env.DATABASE_URL,
   },
-  migrate: {
-    databaseUrl: process.env.DIRECT_URL, // <--- แก้เป็น DIRECT_URL ตรงนี้ครับ
-  },
   database: {
     url: process.env.DATABASE_URL,
     directUrl: process.env.DIRECT_URL,
