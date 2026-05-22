4284be9aac6bf39fbe119f9b6733b4a872fe5463
midsummer-reverie <cccecilia.tz@gmail.com>
2026-05-23 05:57:41 +0700

    Fix loading.tsx by making it a Client Component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,5 @@
-// app/loading.tsx
+"use client"; // <--- เติมบรรทัดนี้ไว้บนสุดของไฟล์เลยครับ!
+
 export default function Loading() {
   return (
     <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-[#020617]">
